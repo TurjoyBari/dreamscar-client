4d35625f226e45c1ece0228a355e4f2c10fa91c3
TurjoyBari <bariturjo@gmail.com>
2026-05-20 15:41:49 +0600

    add jwt and proxy

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-
+import { jwt } from 'better-auth/plugins';
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("dreamscardb");
 
@@ -13,6 +13,14 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true
   },
+  session: {
+    cookieCache: {
+      enabled: true,
+      strategy: 'jwt',
+      maxAge: 5 * 24 * 60 * 60, // in second
+    },
+  },
+  plugins: [jwt()],
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
