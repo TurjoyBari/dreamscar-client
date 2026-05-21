5c41a563fde3d617aa41b1cd14a7c141fe415b13
TurjoyBari <bariturjo@gmail.com>
2026-05-21 21:10:08 +0600

    fix4

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: [
-      "images.unsplash.com","unsplash.com",
-      "lh3.googleusercontent.com","plus.unsplash.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+      },
     ],
   },
 };
